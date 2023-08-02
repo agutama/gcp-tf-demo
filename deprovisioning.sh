63088631a78186294ea6c002de23470ef09440ad
agutama <aguswibawabudi@gmail.com>
2023-08-02 11:18:18 +0700

    fix deprovisioning script

--- deprovisioning.sh
+++ deprovisioning.sh
@@ -1,55 +1,55 @@
 #!/bin/bash
 
 sleep 1
-cd gcp-terraform/compute-engine/env/dev/pritunl/
+cd compute-engine/env/dev/pritunl/
 terraform destroy -auto-approve
-cd ../../../../../
+cd -
 
 sleep 1
-cd gcp-terraform/compute-engine/env/dev/jenkins/
+cd compute-engine/env/dev/jenkins/
 terraform destroy -auto-approve
-cd ../../../../../
+cd -
 
 sleep 1
-cd gcp-terraform/compute-engine/env/dev/prometheus/
+cd compute-engine/env/dev/prometheus/
 terraform destroy -auto-approve
-cd ../../../../../
+cd -
 
 sleep 1
-cd gcp-terraform/compute-engine/env/dev/backend/
+cd compute-engine/env/dev/backend/
 terraform destroy -auto-approve
-cd ../../../../../
+cd -
 
 sleep 1
-cd gcp-terraform/compute-engine/env/dev/nfs-server/
+cd compute-engine/env/dev/nfs-server/
 terraform destroy -auto-approve
-cd ../../../../../
+cd -
 
 sleep 1
-cd gcp-terraform/firewall/env/dev/
+cd firewall/env/dev/
 terraform destroy -auto-approve
-cd ../../../../
+cd -
 
 sleep 1
-cd gcp-terraform/network/env/dev/cloud-nat/
+cd network/env/dev/cloud-nat/
 terraform destroy -var-file="../../../vpc.tfvars" -auto-approve
-cd ../../../../../
+cd -
 
 sleep 1
-cd gcp-terraform/network/env/dev/vpc/
+cd network/env/dev/vpc/
 terraform destroy -var-file="../../../vpc.tfvars" -auto-approve
-cd ../../../../../
+cd -
 
 sleep 1
-cd gcp-terraform/svc-account/env/dev/pritunl
+cd svc-account/env/dev/pritunl
 terraform destroy -auto-approve
-cd ../../../../../
+cd -
 
 sleep 1
-cd gcp-terraform/svc-account/env/dev/prometheus
+cd svc-account/env/dev/prometheus
 terraform destroy -auto-approve
-cd ../../../../../
+cd -
 
-cd gcp-terraform/storage/tf-state/
-terraform destroy -var-file="../gcs.tfvars" -auto-approve
-cd ../../../
+cd storage/env/dev/tf-state/
+terraform destroy -var-file="../../../gcs.tfvars" -auto-approve
+cd -
